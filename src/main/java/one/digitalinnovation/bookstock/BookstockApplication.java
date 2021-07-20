b6f2bc446a3737d90da6dd78fea1736185db480d
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-07-20 20:00:27 -0300

    Project started

---
+++ src/main/java/one/digitalinnovation/bookstock/BookstockApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.bookstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/bookstock/BookstockApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.bookstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
